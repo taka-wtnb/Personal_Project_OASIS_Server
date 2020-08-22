85a90d58ae5fb918270c11ffdce55c59dc807ce4
taka-wtnb <wa040280@gmail.com>
2020-08-21 18:36:19 -0700

    added the otd line chart by item route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const {pool} = require('./config')
 const {otdLineChartQuery} = require('./otd_line_chart_query')
+const {otdLineChartByItemQuery} = require('./otd_line_chart_by_item_query')
 const {otdPieChartQuery} = require('./otd_pie_chart_query')
 const {otdTableQuery} = require('./otd_table_query')
 const {costReductionLineChartQuery} = require('./cost_reduction_line_chart_query')
@@ -13,13 +14,6 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(cors());
-//app.use(bodyParser.json());
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-
-
 
 const getSuppliers = (request, response) => {
   pool.query('SELECT * FROM supplier ORDER BY supplier_name ASC', (error, results) => {
@@ -45,7 +39,20 @@ const getOTDLineChart = (request, response) => {
   const end = request.query.end;
 
   pool.query(otdLineChartQuery, [supplierId, start, end], (error, results) => {
-// console.log(request.query); 
+    if (error) { 
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const getOTDLineChartByItem = (request, response) => { 
+  const supplierId = parseInt(request.query.supplierId);
+  const itemId = parseInt(request.query.itemId);
+  const start = request.query.start;
+  const end = request.query.end;
+
+  pool.query(otdLineChartByItemQuery, [supplierId, itemId, start, end], (error, results) => {
     if (error) { 
       throw error
     }
@@ -59,7 +66,6 @@ const getOTDPieChart = (request, response) => {
   const end = request.query.end;
 
   pool.query(otdPieChartQuery, [supplierId, start, end], (error, results) => {
-// console.log(request.query); 
     if (error) { 
       throw error
     }
@@ -73,7 +79,6 @@ const getOTDTable = (request, response) => {
   const end = request.query.end;
 
   pool.query(otdTableQuery, [supplierId, start, end], (error, results) => {
-// console.log(request.query); 
     if (error) { 
       throw error
     }
@@ -98,22 +103,15 @@ const getCostReductionLineChart = (request, response) => {
 app.get('/suppliers', getSuppliers);
 app.get('/items', getItems);
 app.get('/otdlinechart/', getOTDLineChart);
+app.get('/otdlinechartbyitem/', getOTDLineChartByItem);
 app.get('/otdpiechart/', getOTDPieChart);
 app.get('/otdtable/', getOTDTable);
 app.get('/costreductionlinechart/', getCostReductionLineChart);
 
-// app
-//   .route('/app/dashboard')
-//   // GET endpoint
-//   .get(getSuppliers)
-//   // POST endpoint
-//   //.post(addBook)
-
-
 app.listen(3002, () => {
-  console.log("Example app listening");
+  console.log("Server is listening");
 });
 
-// app.listen(process.env.PORT || 3000, () => {
-//   console.log("Example app listening");
+// app.listen(process.env.PORT || 3002, () => {
+//   console.log("Server is listening");
 // });
---
+++ otd_line_chart_by_item_query.js
@@ -0,0 +1,29 @@
+const otdLineChartByItemQuery = `SELECT  DATE_PART('year', delivery_date) AS Year,
+DATE_PART('month', delivery_date) AS Month,
+TO_CHAR(date_trunc('month', delivery_date), 'Mon-yy') AS MONYY,
+COUNT(id) AS Total,
+SUM(
+CASE WHEN isDelayed = FALSE
+    THEN 1
+    ELSE 0
+END
+) AS OTDs
+FROM    (
+SELECT id, delivery_date, isdelayed
+FROM otd
+INNER JOIN (
+SELECT order_id
+FROM orders
+WHERE supplier_id = $1
+AND item_id = $2
+) AS supplier_orders
+ON supplier_orders.order_id = otd.order_id
+) AS supplier_otds
+WHERE   supplier_otds.delivery_date >= $3 
+AND     supplier_otds.delivery_date <= $4
+GROUP BY Year, Month, MONYY
+ORDER BY Year, Month;`;
+
+module.exports = {
+    otdLineChartByItemQuery,
+}
