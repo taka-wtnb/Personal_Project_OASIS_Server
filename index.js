5f4a0038386be1f42d4ee6cc12cce5ee4a5715d2
taka-wtnb <wa040280@gmail.com>
2021-03-13 14:45:22 -0800

    renamed a route source file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const {pool} = require('./config');
 
 const allItems = require('./routes/allItems');
-const allSuppliers = require('./routes/allSuppliers');
+const allSuppliers = require('./routes/allSuppliersRev');
 const newOrderEntry = require('./routes/newOrderEntry');
 const mostRecentOrderByItemAndSupplier = require('./routes/mostRecentOrderByItemAndSupplier');
 const priceChangeCategories = require('./routes/priceChangeCategories');
