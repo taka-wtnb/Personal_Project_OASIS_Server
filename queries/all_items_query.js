41f7426755cc41b580bc48af8780d2d510d8cfd3
taka-wtnb <wa040280@gmail.com>
2021-03-13 09:37:05 -0800

    updated all items route

--- queries/all_items_query.js
+++ queries/all_items_query.js
@@ -1,15 +1,10 @@
-const allItemsQuery = `SELECT 
-supplier_orders.item_id, item_num, item_name, supplier_orders.unit_price, supplier_orders.qty, supplier_orders.order_date
-FROM item
+const allItemsQuery = `SELECT
+item_info.item_num, item_info.item_name, item_info.item_type, item_unit.unit
+FROM item_unit
 INNER JOIN (
-SELECT item_id, qty, unit_price, order_date
-FROM orders
-WHERE supplier_id = $1
-AND item_id = $2
-AND order_date >= $3
-AND order_date <= $4
-) AS supplier_orders
-ON supplier_orders.item_id = item.id;`;
+SELECT item_num, item_name, item_type, unit_id FROM item ORDER BY item_num ASC
+) AS item_info
+ON item_info.unit_id = item_unit.id;`;
 
 module.exports = {
     allItemsQuery,
