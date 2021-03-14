56e51183380f6de3bb75ca9c502298499f04af4a
taka-wtnb <wa040280@gmail.com>
2021-03-14 09:07:33 -0700

    updated a couple of queries

--- queries/all_suppliers_query.js
+++ queries/all_suppliers_query.js
@@ -2,9 +2,9 @@ const allSuppliersQuery = `SELECT
 supplier_info.id, supplier_info.supplier_code, supplier_info.supplier_name, supplier_type.type
 FROM supplier_type
 INNER JOIN (
-SELECT id, supplier_code, supplier_name, supplier_type_id FROM supplier ORDER BY supplier_code ASC
+SELECT id, supplier_code, supplier_name, supplier_type_id FROM supplier
 ) AS supplier_info
-ON supplier_info.supplier_type_id = supplier_type.id;`;
+ON supplier_info.supplier_type_id = supplier_type.id ORDER BY supplier_info.supplier_code ASC;`;
 
 module.exports = {
     allSuppliersQuery,
--- queries/most_recent_order_by_item_and_supplier_query.js
+++ queries/most_recent_order_by_item_and_supplier_query.js
@@ -1,6 +1,6 @@
 const mostRecentOrderByItemAndSupplierQuery = `SELECT * FROM orders 
 WHERE supplier_id = $2 AND item_id = $1 
-ORDER BY order_date DESC LIMIT 2;`;
+ORDER BY order_date DESC, order_id DESC LIMIT 2;`;
 
 module.exports = {
     mostRecentOrderByItemAndSupplierQuery,
